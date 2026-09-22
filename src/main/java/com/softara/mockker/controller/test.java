f9be5b708681ed042397e0e4ad8b1c75a0b77606
PrAtHaM KhObRaGaDe <prathamkhobragade05@gmail.com>
2026-09-22 20:54:25 +0530

    testing

--- src/main/java/com/softara/mockker/controller/test.java
+++ src/main/java/com/softara/mockker/controller/test.java
@@ -1,24 +1,13 @@
 package com.softara.mockker.controller;
 
-import java.util.List; 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping("mockker")
-public class AdminController {
-	
-	@Autowired
-	AdminService adminService;
+public class Test {
 	 
 	@GetMapping("/test")
 	public String getOverviewCount(){	
